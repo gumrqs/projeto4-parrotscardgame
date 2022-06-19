d6faa829bf93d8b2b9fdb95a70001ca1095483e0
Gustavo <gumrqss@gmail.com>
2022-06-18 23:16:21 -0300

    inicio de colocar imagens

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function validarNumeroCartas(){
     numeroCartas= prompt("Com quantas cartas deseja jogar?(4 à 14)");
 // condições de jogo:
 //  precisa ser apenas numeros pares %2===0
-// precisa ser sombre numeros 
+// precisa ser sobre numeros 
 // precisa ser numero maior que 4 e menor que 15
     if(numeroCartas < 4 || numeroCartas >15)
     {
@@ -29,13 +29,12 @@ for(let index=0; index<numeroCartas; index++){
     `<div class="cartas">
         
         <div class="frente face" onclick="selecionar(this)">
+        <img class="imagens" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png" alt="">
         </div>
         <div class="verso face" onclick="selecionar(this)">
-          Verso
+        <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/fiestaparrot.gif" alt="">
         </div>
       </div>`
     document.querySelector(".container-cartas").innerHTML+= cartasTemplate;   
 }
-
-
 
