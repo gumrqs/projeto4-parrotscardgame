87cac09118f98ac65db88eae3a224c005c45bc5f
Gustavo <gumrqss@gmail.com>
2022-06-18 22:51:25 -0300

    adicionar cards e responsive

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let cartas=[];
+const cartas=[];
 let numeroCartas;
 
-function avaliarJogo(){
-    numeroCartas= document.querySelector(".cartas");
-numeroCartas= prompt("Com quantas cartas deseja jogar?(4 à 14)");
+function validarNumeroCartas(){
+    
+    numeroCartas= prompt("Com quantas cartas deseja jogar?(4 à 14)");
 // condições de jogo:
 //  precisa ser apenas numeros pares %2===0
 // precisa ser sombre numeros 
 // precisa ser numero maior que 4 e menor que 15
-    if(numeroCartas < 4 || numeroCartas >15){
-    alert("Esse modo não é permitido, tente novamente");
-    avaliarJogo();
+    if(numeroCartas < 4 || numeroCartas >15)
+    {
+         alert("Esse modo não é permitido, tente novamente");
+         validarNumeroCartas();
 
-}        if ( numeroCartas%2 === 0){
+    }        
+        if ( numeroCartas%2 === 0)
+                {
                 alert("tudo certo, bora bora");
-                numeroCartas=numeroCartas;
-                alert(numeroCartas);
-                                   }
+                }
             else { 
                 alert("Só números pares por favor né, nem adianta botar letras...");
-                   avaliarJogo();}
+                validarNumeroCartas();}
                  }
-avaliarJogo();
+ validarNumeroCartas();
 
-for( numeroCartas; numeroCartas<6 ; numeroCartas++){
-    alert(help);
-
+for(let index=0; index<numeroCartas; index++){
+    const cartasTemplate= 
+    `<div class="cartas">
+        
+        <div class="frente face" onclick="selecionar(this)">
+        </div>
+        <div class="verso face" onclick="selecionar(this)">
+          Verso
+        </div>
+      </div>`
+    document.querySelector(".container-cartas").innerHTML+= cartasTemplate;   
 }
+
 
 
